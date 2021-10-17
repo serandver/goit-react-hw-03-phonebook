5a65678c7cee53aaef73c9d9e4d9e76594231ace
Serhii Khomenko <sakhom16@gmail.com>
2021-10-17 18:59:22 +0300

    add localStorage as contact storage

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form/Form';
+import { v4 as uuidv4 } from 'uuid';
+import Contact from './Contact';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleAddContact = data => {
+    const { name, number } = data;
+    const id = uuidv4();
+    const newContact = new Contact(id, name, number);
+    if (this.state.contacts.find(contact => newContact.equals(contact))) {
+      alert(`${newContact.name} already exists in contacts`);
+      return;
+    }
+    this.setState({
+      contacts: [...this.state.contacts, newContact],
+    });
+  };
+
+  handleDeleteContact = idContact => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== idContact),
+    }));
+  };
+
+  changeFilter = event => {
+    const filterValue = event.currentTarget.value;
+    this.setState({ filter: filterValue });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    if (filter) {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase()),
+      );
+    } else {
+      return contacts;
+    }
+  };
+
+  componentDidMount() {
+    console.log('Component did mount');
+    const contactsString = localStorage.getItem('contacts');
+    const contacts = JSON.parse(contactsString);
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component did update');
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <Form onSubmit={this.handleAddContact} />
+        {filteredContacts.length > 0 && (
+          <div>
+            <h2>Contacts</h2>
+            <Filter value={filter} onChange={this.changeFilter} />
+            <ContactList
+              contacts={filteredContacts}
+              onDeleteContact={this.handleDeleteContact}
+            />
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <p>Name</p>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+          <p>Number</p>
+          <input
+            type="tel"
+            value={this.state.number}
+            onChange={this.handleChange}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+          />
+          <br />
+          <button type="submit" onClick={this.handleChange}>
+            Add contact
+          </button>
+        </label>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Form;
